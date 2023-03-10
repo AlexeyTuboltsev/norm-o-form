9cc6b8433791532f880e232635f8de3d184cae3c
AlexeyTuboltsev <tblz@palavara.com>
2023-03-10 12:18:26 +0100

    style array components

--- examples/array/src/form/components/ArrayActions.tsx
+++ examples/array/src/form/components/ArrayActions.tsx
@@ -1,29 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { EFormActionType } from "../formReducer";
 import * as React from "react";
 import cn from "classnames";
 import styles from "./Button.module.scss";
+import { faCaretUp, faCaretDown, faMinus, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
 
-type TButtonAction = {type: EFormActionType}
+type TButtonAction = { type: EFormActionType }
 
-const ArrayButton:React.FunctionComponent<{style?:string, label:string, id:string}> = ({style, id,label}) => {
+const ArrayButton: React.FunctionComponent<{ style?: string, id: string, children?: React.ReactNode }> = ({
+  style,
+  id,
+  children
+}) => {
   return <button
     id={id}
-    className={cn(styles.arrayButtonBase,style)}
-  >    {label}
+    className={cn(styles.arrayButtonBase, style)}
+  >
+    {children}
   </button>
 }
 
-export const AddArrayMemberFormButton:React.FunctionComponent<{id:string}> = ({id}) =>
-  <ArrayButton
-    style={styles.addArrayMemberButton}
-    id={id}
-    label="+"
-  />
+export const AddArrayMemberFormButton: React.FunctionComponent<{ id: string }> = ({ id }) =>
+  <ArrayButton style={styles.addArrayMemberButton} id={id}>
+    <FontAwesomeIcon size={"sm"} icon={faPlus} />
+  </ArrayButton>
 
-export const DeleteArrayMemberFormButton:React.FunctionComponent<{id:string}> = ({id}) =>
-  <ArrayButton label="X" id={id} />
+export const DeleteArrayMemberFormButton: React.FunctionComponent<{ id: string }> = ({ id }) =>
+  <ArrayButton id={id}>
+    <FontAwesomeIcon size={"sm"} icon={faXmark} />
+  </ArrayButton>
 
-export const MoveUpArrayMemberFormButton:React.FunctionComponent<{id:string}> = ({id}) =>
-  <ArrayButton label="^" id={id} />
-export const MoveDownArrayMemberFormButton:React.FunctionComponent<{id:string}> = ({id}) =>
-  <ArrayButton label="v" id={id} />
+export const MoveUpArrayMemberFormButton: React.FunctionComponent<{ id: string }> = ({ id }) =>
+  <ArrayButton id={id}>
+    <FontAwesomeIcon size={"sm"} icon={faCaretUp} />
+  </ArrayButton>
+
+export const MoveDownArrayMemberFormButton: React.FunctionComponent<{ id: string }> = ({ id }) =>
+  <ArrayButton id={id}>
+    <FontAwesomeIcon size={"sm"} icon={faCaretDown} />
+  </ArrayButton>
