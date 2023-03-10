45d74d4db1e7748195476a9cb51d45ae051a7647
AlexeyTuboltsev <tblz@palavara.com>
2023-03-10 23:48:52 +0100

    add some explanatory text

--- examples/array/src/form/components/FormView.tsx
+++ examples/array/src/form/components/FormView.tsx
@@ -10,18 +10,18 @@ export const FormView: React.FunctionComponent<{ viewTab: ETab, form: TFormData,
   switch (viewTab) {
     case ETab.FORM: {
       return <div className={styles.tabContent}>
-        <div>form</div>
+        <div className={styles.text}>form</div>
         <ArrayExampleForm formId={ARRAY_EXAMPLE_FORM_ROOT} formData={form} />
       </div>
     }
     case ETab.FORM_FUNCTION:
       return <div className={styles.tabContent}>
-        <div>form definition function</div>
+        <div className={styles.text}>Form definition function is the starting point of the form. </div>
         <CodeTab code={formFunctionView} />
       </div>
     case ETab.FORM_GENERATOR:
       return <div className={styles.tabContent}>
-        <div>form generator</div>
+        <div className={styles.text}>”Form generator” is what a form definition function returns — an object that is kept in memory during the entire form lifecycle. It's a key-value map of ”field generators” that hold functions for generation and validation of each node. Each node keeps a link to its fieldGenerator</div>
         <CodeTab code={formGeneratorView} />
       </div>
   }
